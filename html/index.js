4516e3ddec82a3d3f1b9671454c655c6399e6e73
ItsLeMax <lemaxyt1243@gmail.com>
2024-04-08 08:44:36 +0200

    Added age calculation

--- html/index.js
+++ html/index.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    for (const year of document.getElementsByClassName("year")) {
-        year.textContent = new Date().getFullYear() - year.textContent;
+    for (const birthdayElement of document.getElementsByClassName("bday")) {
+        const bday = birthdayElement.textContent.split(".");
+        const bdate = new Date(bday[2], parseInt(bday[1]) - 1, bday[0]);
+        const today = new Date();
+
+        let age = today.getFullYear() - bdate.getFullYear();
+        const month = today.getMonth() - bdate.getMonth();
+
+        if (month < 0 || (month == 0 && today.getDate() < bdate.getDate())) age--;
+        birthdayElement.textContent = age;
     }
 })
