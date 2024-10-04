6c2f56f8b060c79fbee7422facff19b1b0b7e93f
ItsLeMax <lemaxyt1243@gmail.com>
2024-10-04 17:09:15 +0200

    removed old CSS, new page structure, better wording, better backend, QoL

--- team/apply.js
+++ team/apply.js
@@ -1,19 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const create = document.getElementsByTagName("button")[0];
+    const create = document.querySelector("button");
+    const checkboxes = document.querySelectorAll("input");
 
     create.addEventListener("click", () => {
-        let mailTo = "mailto:itslemax.media@gmail.com?subject=FPM-Bewerbung%20als%20SELECTED_JOBS&body=-----------%0D%0A%C3%9Cber%20dich%3A%0D%0A%0D%0A%0D%0A---------------------------------------%0D%0ABeschreibung%20der%20verf%C3%BCgbaren%20Zeit%3A%0D%0A%0D%0A%0D%0A---------------------%0D%0ADiscord%20%40-Handle%3A%0D%0A";
+        let mailTo = (
+            "mailto:" + "itslemax.media@gmail.com" +
+            "?subject=" + ("FPM-Bewerbung" + "%20" + "als" + "%20" + "SELECTION") +
+            "&body=" + "-----------" +
+            "%0D%0A" + ("%C3%9Cber" + "%20" + "mich") + "%3A%0D%0A%0D%0A%0D%0A" +
+            "---------------------------------------" +
+            "%0D%0A" + ("Beschreibung" + "%20" + "der" + "%20" + "verf%C3%BCgbaren" + "%20" + "Zeit") + "%3A%0D%0A%0D%0A%0D%0A" +
+            "---------------------" +
+            "%0D%0A" + ("Discord" + "%20" + "%40-Handle") + "%3A%0D%0A@"
+        );
 
-        const job = new Array;
-        if (document.getElementById("builder").checked) job.push("Builder");
-        if (document.getElementById("editor").checked) job.push("Editor");
+        const jobs = new Array;
 
-        mailTo = mailTo.replace("SELECTED_JOBS", job.join("%20%26%20"));
+        for (const checkbox of checkboxes) {
+            if (!checkbox.checked) continue;
+
+            const job = checkbox.id;
+            jobs.push(job.charAt(0).toUpperCase() + job.slice(1));
+        }
+
+        mailTo = mailTo.replace("SELECTION", jobs.join("%20%26%20"));
 
         window.open(mailTo);
     })
 
-    document.addEventListener("click", () => {
-        create.disabled = !document.getElementById("builder").checked && !document.getElementById("editor").checked;
+    document.getElementById("jobs").addEventListener("click", () => {
+        for (const checkbox of checkboxes) {
+            if (checkbox.checked) {
+                create.disabled = false;
+                break;
+            }
+
+            create.disabled = true;
+        }
     })
 })
