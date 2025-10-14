aea80eab55e29e7d50cfb1f2813a040cc8e7ece7
ItsLeMax <lemaxyt1243@gmail.com>
2025-10-14 16:12:11 +0200

    refactor: update jobs, add images and comments

--- team/apply.js
+++ team/apply.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const create = document.querySelector("button");
     const checkboxes = document.querySelectorAll("input");
 
+    // E-Mail creation process
+
     create.addEventListener("click", () => {
+
         let mailTo = (
             "mailto:" + "itslemax.media@gmail.com" +
-            "?subject=" + ("FPM-Bewerbung" + "%20" + "als" + "%20" + "SELECTION") +
+            "?subject=" + ("FPM-Bewerbung" + "%20" + "als" + "%20" + "%SELECTION%") +
             "&body=" + "-----------" +
             "%0D%0A" + ("%C3%9Cber" + "%20" + "mich") + "%3A%0D%0A%0D%0A%0D%0A" +
             "---------------------------------------" +
@@ -17,25 +21,34 @@ document.addEventListener("DOMContentLoaded", () => {
         const jobs = new Array;
 
         for (const checkbox of checkboxes) {
-            if (!checkbox.checked) continue;
+
+            if (!checkbox.checked)
+                continue;
 
             const job = checkbox.id;
             jobs.push(job.charAt(0).toUpperCase() + job.slice(1));
+
         }
 
-        mailTo = mailTo.replace("SELECTION", jobs.join("%20%26%20"));
+        mailTo = mailTo.replace("%SELECTION%", jobs.join("%20%26%20"));
 
         window.open(mailTo);
+
     })
 
     document.getElementById("jobs").addEventListener("click", () => {
+
         for (const checkbox of checkboxes) {
+
             if (checkbox.checked) {
                 create.disabled = false;
                 break;
             }
 
             create.disabled = true;
+
         }
+
     })
+
 })
