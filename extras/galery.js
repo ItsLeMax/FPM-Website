aa57098f0b2e509a251db3aa78374d8687d8a76d
ItsLeMax <lemaxyt1243@gmail.com>
2025-10-16 19:23:26 +0200

    style: add comments, update formatting, remove german docs

--- extras/galery.js
+++ extras/galery.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const select = document.querySelector("select");
 
     const image = document.getElementById("image");
     const loading = document.getElementById("loading");
     const caption = document.getElementById("caption");
 
+    // Change of project
+
     select.addEventListener("change", () => {
+
+        // Show caption only if the buttons are disabled (I forgot the reason why I did this)
+
         updateButtons((button) => {
             if (button.disabled) {
                 caption.style.display = "block";
             }
         });
+
+        // Load page count and image
 
         document.getElementById("index").style.display = "block";
         const pageNumbers = getPageCount().pageNumbers;
@@ -21,13 +29,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
         XMLHttpRequests({
             readystate: (xhr) => {
+
+                // Set maximum page count
+
                 const response = JSON.parse(xhr.responseText);
-                if (!response) return;
+
+                if (!response)
+                    return;
 
                 const pageNumbers = getPageCount().pageNumbers;
                 pageNumbers[1] = response.length;
 
                 updatePageCount(pageNumbers);
+
             }
         }, {
             subdomain: "extras",
@@ -35,41 +49,65 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     })
 
+    // Image switcher logic
+
     updateButtons((button) => {
         button.addEventListener("click", () => {
+
             const pageNumbers = getPageCount().pageNumbers;
 
-            if (pageNumbers[0] == 1 && button.id == "previous") return;
-            if (pageNumbers[0] == pageNumbers[1] && button.id == "next") return;
+            // Disallow going to page 0 or lower
+
+            if (pageNumbers[0] == 1 && button.id == "previous")
+                return;
+
+            // Disallow going to page higher than maximum
+
+            if (pageNumbers[0] == pageNumbers[1] && button.id == "next")
+                return;
+
+            // Increment or decrement page number depending on the clicked button
+
             pageNumbers[0] = button.id == "next" ? pageNumbers[0] += 1 : pageNumbers[0] -= 1;
             updatePageCount(pageNumbers);
 
-            if (!select.value) return;
+            if (!select.value)
+                return;
+
             updateImage();
+
         })
     });
 
+    // Change of image
+
     image.addEventListener("load", () => {
+
+        // Update image description text content
+
         caption.innerText = eval(getSelectId())[getPageCount().pageNumbers[0]] || "";
+
         updateButtons((button) => {
+
+            // Allow page switching again after image is loaded
+
             button.disabled = false;
+
+
             image.style.filter = "unset";
+
+            // Remove loading animation
+
             loading.style.display = "none";
+
         });
+
     })
 
     /**
-     * @description
-     * Führt eine Funktion für alle Knöpfe aus
-     *
-     * Executes a function for all buttons
-     *
+     * @description Executes a function for all buttons
      * @author ItsLeMax
-     *
-     * @param { Function } fn
-     * Funktion zum Ausführen
-     *
-     * Function to execute
+     * @param { Function } fn Function to execute
      */
     function updateButtons(fn) {
         for (const buttonId of ["next", "previous"]) {
@@ -78,19 +116,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     /**
-     * @description
-     * Entnimmt den Text oben links, welcher den Fortschritt darstellt
-     * 
-     * Retrieves the text on the top left, which shows the progress
-
+     * @description Retrieves the text on the top left, which shows the progress
      * @author ItsLeMax
-
-     * @returns
-     * Objekt mit Element und Array mit beiden Zahlen, aktuelle Nummer und Nummer insgesamt oder Element, unbearbeitet
-     * 
-     * Object with element and array with both numbers, current number and total number or element, unedited
+     * @returns Object with element and array with both numbers, current number and total number or element, unedited
      */
     function getPageCount() {
+
         const pageElement = document.getElementById("index");
 
         const pageNumbers = pageElement.innerText.split("/");
@@ -100,20 +131,13 @@ document.addEventListener("DOMContentLoaded", () => {
             pageNumbers: pageNumbers,
             pageElement: pageElement
         };
+
     };
 
     /**
-     * @description
-     * Überschreibt den Text oben links
-     * 
-     * Overwrites the text on the top left
-
+     * @description Overwrites the text on the top left
      * @author ItsLeMax
-
-     * @param { Array<String> } pageNumbers
-     * geteilter Index im Array aus dem Objekt der Funktion `pageCount`
-     * 
-     * splitted index inside the array from the object of the function `pageCount`
+     * @param { Array<String> } pageNumbers Splitted index inside the array from the object of the function `pageCount`
      */
     function updatePageCount(pageNumbers) {
         pageNumbers[0] = pageNumbers[0].toString();
@@ -121,36 +145,28 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     /**
-     * @description
-     * Aktualisiert das Bild, entsprechend des Indexes
-     * 
-     * Updates the image, according to the index
-
+     * @description Updates the image, according to the index
      * @author ItsLeMax
      */
     function updateImage() {
+
         image.setAttribute("src", "https://media.fpm-studio.de/movies/" + getSelectId().toLowerCase() + "/" + getPageCount().pageNumbers[0] + ".png");
+
         updateButtons((button) => {
             button.disabled = true;
             image.style.filter = "brightness(33%) blur(.1rem)";
             loading.style.display = "block";
         });
+
     };
 
     /**
-     * @description
-     * Entnimmt die Select-ID
-     * 
-     * Takes out the select id
-
+     * @description Takes out the select id
      * @author ItsLeMax
-
-     * @returns { String }
-     * String mit Select-ID
-     * 
-     * String with select id
+     * @returns { String } String with select id
      */
     function getSelectId() {
         return select.value.replaceAll(" ", "_");
     }
+
 })
