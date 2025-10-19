df4e9b8cd1174ac4dcc8eafe25af6591a5c5d962
ItsLeMax <lemaxyt1243@gmail.com>
2025-10-19 15:52:50 +0200

    style: add comments and reformat

--- dev/countdown/settings.js
+++ dev/countdown/settings.js
@@ -1,53 +1,86 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const time = document.getElementById("minutes");
     const start = document.querySelector("button");
+
     const initialText = start.innerText;
     const initialColor = start.style.backgroundColor;
 
     time.addEventListener("input", () => {
-        if (time.value < 0) {
+
+        // Disallow negative numbers
+
+        if (time.value < 0)
             time.value = 0;
-        }
 
-        if (time.value == 100) {
+        // Disallow beyond 99 minutes (Who tf needs that shit)
+
+        if (time.value == 100)
             time.value = 99;
-        }
 
-        if (time.value.length > 2) {
+        // Disallow adding a third number (restricted to two digits)
+
+        if (time.value.length > 2)
             time.value = time.value.slice(0, 2);
-        }
+
+        // Disallow beyond 60 and 0 minutes but highlight text in that case instead of restricting it
 
         const exceedsLimit = time.value > 60 || time.value < 1;
+
         time.style.color = exceedsLimit ? "red" : "black";
         start.disabled = time.value ? exceedsLimit : false;
+
     })
 
     const alignments = ["left", "center", "right"];
+
     for (const alignment_a of alignments) {
+
+        // Add click listener for each alignment checkbox
+
         document.getElementById(alignment_a).addEventListener("click", () => {
+
+            // If the checkbox is not checked: check the first checkbox (left) as default
+
             if (!document.getElementById(alignment_a).checked) {
                 document.getElementById(alignments[0]).checked = true;
                 return;
             }
+
+            // Uncheck all other checkboxes
+
             for (const alignment_b of alignments) {
-                if (alignment_a != alignment_b) {
+
+                if (alignment_a != alignment_b)
                     document.getElementById(alignment_b).checked = false;
-                }
+
             }
+
         })
+
     }
 
     start.addEventListener("click", () => {
+
+        // Update start button depending on state
+
         start.style.backgroundColor = start.innerText == initialText ? "rgb(255, 110, 110)" : initialColor;
         start.innerText = start.innerText == initialText ? "Stop" : initialText;
+
+        // Cache settings to use in the separate overlay
 
         localStorage.setItem("toggle", JSON.stringify({
             init: start.innerText != initialText,
             time: parseInt(time.value) || 10,
         }));
+
     })
 
     document.getElementById("update").addEventListener("click", () => {
+
+        // Click on "update" will update the cache
+        // 'align' executes an inlined function immediately to look for and return the checked alignment as value
+
         localStorage.setItem("update", JSON.stringify({
             color: document.getElementById("picker").value,
             align: (() => {
@@ -58,6 +91,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })(),
             font: document.getElementsByTagName("select")[0].value
-        }))
+        }));
+
     })
+
 })
--- dev/winrate/settings.js
+++ dev/winrate/settings.js
@@ -1,25 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Target Win/Loss inputs
+
     for (const query of ["#wl-wins input", "#wl-losses input"]) {
+
         const element = document.querySelector(query);
 
-        element.addEventListener("input", () => {
-            if (parseInt(element.value) < 0 || !element.value) {
-                element.value = 0;
-            }
+        // Add event listener to them
 
-            if (parseInt(element.value) > 999) {
+        element.addEventListener("input", () => {
+
+            // Set limits to prevent unrealistic values
+
+            if (parseInt(element.value) < 0 || !element.value)
+                element.value = 0;
+
+            if (parseInt(element.value) > 999)
                 element.value = 999;
-            }
+
+            // Get picked wins and losses
 
             const wins = parseInt(document.querySelector("#wl-wins input").value);
             const losses = parseInt(document.querySelector("#wl-losses input").value);
+
+            // Calculate win/loss percentage
 
             const winLossPercentage = (((wins / (wins + losses)) * 100) || 0).toFixed();
             const winColor = "rgb(255, 153, 51)";
             const lossColor = "rgb(51, 153, 255)";
 
+            // Apply two color gradient with calculated values
+
             document.querySelector("#wl-average b").innerText = `${winLossPercentage}%`;
             document.querySelector("#wl-average span").style.background = (!wins && !losses) ? "gray" : `linear-gradient(to right, ${winColor} 0%, ${winColor} ${winLossPercentage}%, ${lossColor} ${winLossPercentage}%, ${lossColor} 100%`
+
         })
+
     }
+
 })
