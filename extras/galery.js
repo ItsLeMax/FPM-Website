ae785548e76c83f5d9b3f3bfda8f2e6f38eaa19c
ItsLeMax <lemaxyt1243@gmail.com>
2026-07-24 23:20:05 +0200

    refactor: rewrite entire file

--- extras/galery.js
+++ extras/galery.js
@@ -6,186 +6,219 @@ document.addEventListener("DOMContentLoaded", () => {
     const loading = document.getElementById("loading");
     const caption = document.getElementById("caption");
 
-    // Gets and sets the movies to choose from
+    requestMovies(select);
 
-    XMLHttpRequests({
-        readystate: (xhr) => {
+    // Listener and handling for changing the movie inside the dropdown and switching images
 
-            const response = JSON.parse(xhr.responseText);
+    select.addEventListener("change", () => changeMovie(image, loading, caption, select));
+    image.addEventListener("load", () => handleImageLoad(select, caption, image, loading));
 
-            for (const movie of response) {
-                const text = movie.split("_").map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
-                select.add(new Option(text));
-            }
-
-        },
-
-    }, {
-        subdomain: "extras",
-        sentData: null
+    updateButtons((button) => {
+        button.addEventListener("click", () => switchImage(button, select, image, loading));
     });
 
-    // Change of project
+});
 
-    select.addEventListener("change", () => {
+/**
+ * @description Gets and sets the movies to choose from
+ * @author ItsLeMax
+ * @param { HTMLSelectElement } select
+ */
+function requestMovies(select) {
 
-        // Shows the text block (caption) if a movie is selected
+    const readystate = (xhr) => {
 
-        updateButtons((button) => {
-            if (button.disabled) {
-                caption.style.display = "block";
-            }
-        });
+        const response = JSON.parse(xhr.responseText);
 
-        // Load page count and image
+        for (const movie of response) {
+            const text = movie.split("_").map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
+            select.add(new Option(text));
+        }
 
-        document.getElementById("index").style.display = "block";
+    };
+
+    new RequestToFPM({ subdomain: Subdomain.EXTRAS }, { readystate });
+
+}
+
+/**
+ * @description Acts if the movie was changed inside the dropdown selection menu
+ * @param { HTMLElement } image
+ * @param { HTMLElement } loading
+ * @param { HTMLElement } caption
+ * @param { HTMLSelectElement } select 
+ */
+function changeMovie(image, loading, caption, select) {
+
+    // Shows the text block (caption) if a movie is selected
+
+    updateButtons((button) => {
+        if (button.disabled) {
+            caption.style.display = "block";
+        }
+    });
+
+    // Load page count and image
+
+    document.getElementById("index").style.display = "block";
+    const pageNumbers = getPageCount().pageNumbers;
+    pageNumbers[0] = 1;
+
+    updatePageCount(pageNumbers);
+    updateImage(image, loading, select);
+
+    const readystate = (xhr) => {
+
+        // Set maximum page count
+
+        const response = JSON.parse(xhr.responseText);
+
+        if (!response)
+            return;
+
         const pageNumbers = getPageCount().pageNumbers;
-        pageNumbers[0] = 1;
+        pageNumbers[1] = response.length;
 
         updatePageCount(pageNumbers);
-        updateImage();
 
-        XMLHttpRequests({
-            readystate: (xhr) => {
+    };
 
-                // Set maximum page count
+    new RequestToFPM({ subdomain: Subdomain.EXTRAS, sentData: getSelectId(select).toLowerCase() }, { readystate });
 
-                const response = JSON.parse(xhr.responseText);
+}
 
-                if (!response)
-                    return;
+/**
+ * @param { HTMLSelectElement } select
+ * @param { HTMLSelectElement } caption 
+ * @param { HTMLElement } image
+ * @param { HTMLElement } loading
+ */
+function handleImageLoad(select, caption, image, loading) {
 
-                const pageNumbers = getPageCount().pageNumbers;
-                pageNumbers[1] = response.length;
+    // Update image description text content
 
-                updatePageCount(pageNumbers);
+    const selectId = getSelectId(select);
+    const currentPage = getPageCount().pageNumbers[0];
 
-            }
-        }, {
-            subdomain: "extras",
-            sentData: getSelectId().toLowerCase()
-        });
-    })
-
-    // Image switcher logic
+    caption.innerText = eval(selectId)[currentPage] || "";
 
     updateButtons((button) => {
 
-        button.addEventListener("click", () => {
+        // Allow page switching again after image is loaded
 
-            const pageNumbers = getPageCount().pageNumbers;
+        button.disabled = false;
+        image.style.filter = "unset";
 
-            // Disallow going to page 0 or lower
+        // Remove loading animation
 
-            if (pageNumbers[0] == 1 && button.id == "previous")
-                return;
-
-            // Disallow going to page higher than maximum
-
-            if (pageNumbers[0] == pageNumbers[1] && button.id == "next")
-                return;
-
-            // Increment or decrement page number depending on the clicked button
-
-            pageNumbers[0] = button.id == "next" ? pageNumbers[0] += 1 : pageNumbers[0] -= 1;
-            updatePageCount(pageNumbers);
-
-            if (!select.value)
-                return;
-
-            updateImage();
-
-        })
+        loading.style.display = "none";
 
     });
 
-    // Change of image
+}
 
-    image.addEventListener("load", () => {
+/**
+ * @description Switches the image after pressing any image switch button
+ * @author ItsLeMax
+ * @param { HTMLButtonElement } button 
+ * @param { HTMLSelectElement } select
+ * @param { HTMLElement } image
+ * @param { HTMLElement } loading
+ */
+function switchImage(button, select, image, loading) {
 
-        // Update image description text content
+    const pageNumbers = getPageCount().pageNumbers;
 
-        caption.innerText = eval(getSelectId())[getPageCount().pageNumbers[0]] || "";
+    // Disallow going to page 0 or lower
 
-        updateButtons((button) => {
+    if (pageNumbers[0] == 1 && button.id == "previous")
+        return;
 
-            // Allow page switching again after image is loaded
+    // Disallow going to page higher than maximum
 
-            button.disabled = false;
-            image.style.filter = "unset";
+    if (pageNumbers[0] == pageNumbers[1] && button.id == "next")
+        return;
 
-            // Remove loading animation
+    // Increment or decrement page number depending on the clicked button
 
-            loading.style.display = "none";
+    pageNumbers[0] = button.id == "next" ? pageNumbers[0] += 1 : pageNumbers[0] -= 1;
+    updatePageCount(pageNumbers);
 
-        });
+    if (!select.value)
+        return;
 
-    })
+    updateImage(image, loading, select);
 
-    /**
-     * @description Executes a function for all buttons
-     * @author ItsLeMax
-     * @param { Function } fn Function to execute
-     */
-    function updateButtons(fn) {
-        for (const buttonId of ["next", "previous"]) {
-            fn(document.getElementById(buttonId));
-        }
+}
+
+/**
+ * @description Executes a function for all buttons
+ * @author ItsLeMax
+ * @param { Function } callback Callback function to execute
+ */
+function updateButtons(callback) {
+    for (const buttonId of ["next", "previous"]) {
+        callback(document.getElementById(buttonId));
     }
+}
 
-    /**
-     * @description Retrieves the text on the top left, which shows the progress
-     * @author ItsLeMax
-     * @returns Object with element and array with both numbers, current number and total number or element, unedited
-     */
-    function getPageCount() {
+/**
+ * @description Retrieves the text on the top left, which shows the progress
+ * @author ItsLeMax
+ * @returns Object with element and array with both numbers, current number and total number or element, unedited
+ */
+function getPageCount() {
 
-        const pageElement = document.getElementById("index");
+    const pageElement = document.getElementById("index");
 
-        const pageNumbers = pageElement.innerText.split("/");
-        pageNumbers[0] = parseInt(pageNumbers[0]);
+    const pageNumbers = pageElement.innerText.split("/");
+    pageNumbers[0] = parseInt(pageNumbers[0]);
 
-        return {
-            pageNumbers: pageNumbers,
-            pageElement: pageElement
-        };
-
+    return {
+        pageNumbers: pageNumbers,
+        pageElement: pageElement
     };
 
-    /**
-     * @description Overwrites the text on the top left
-     * @author ItsLeMax
-     * @param { Array<String> } pageNumbers Splitted index inside the array from the object of the function `pageCount`
-     */
-    function updatePageCount(pageNumbers) {
-        pageNumbers[0] = pageNumbers[0].toString();
-        getPageCount().pageElement.innerText = pageNumbers.join("/");
-    };
+};
 
-    /**
-     * @description Updates the image, according to the index
-     * @author ItsLeMax
-     */
-    function updateImage() {
+/**
+ * @description Overwrites the text on the top left
+ * @author ItsLeMax
+ * @param { Array<String> } pageNumbers Page number indexes splitted inside an array from the object of the function `pageCount`
+ */
+function updatePageCount(pageNumbers) {
+    pageNumbers[0] = pageNumbers[0].toString();
+    getPageCount().pageElement.innerText = pageNumbers.join("/");
+};
 
-        image.setAttribute("src", "https://media.fpm-studio.de/movies/" + getSelectId().toLowerCase() + "/" + getPageCount().pageNumbers[0] + ".png");
+/**
+ * @description Updates the image, according to the index
+ * @author ItsLeMax
+ * @param { HTMLElement } image
+ * @param { HTMLElement } loading
+ * @param { HTMLSelectElement } select
+ */
+function updateImage(image, loading, select) {
 
-        updateButtons((button) => {
-            button.disabled = true;
-            image.style.filter = "brightness(33%) blur(.1rem)";
-            loading.style.display = "block";
-        });
+    const selection = getSelectId(select).toLowerCase();
+    const currentPage = getPageCount().pageNumbers[0];
 
-    };
+    image.setAttribute("src", `https://media.fpm-studio.de/movies/${selection}/${currentPage}.png`);
 
-    /**
-     * @description Takes out the select id
-     * @author ItsLeMax
-     * @returns { String } String with select id
-     */
-    function getSelectId() {
-        return select.value.replaceAll(" ", "_");
-    }
+    updateButtons((button) => {
+        button.disabled = true;
+        image.style.filter = "brightness(33%) blur(.1rem)";
+        loading.style.display = "block";
+    });
 
-})
+};
+
+/**
+ * @description Takes out the select id
+ * @author ItsLeMax
+ * @param { HTMLSelectElement } select
+ * @returns { String } String with select id
+ */
+function getSelectId(select) {
+    return select.value.replaceAll(" ", "_");
+}
